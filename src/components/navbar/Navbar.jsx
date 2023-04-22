11cdeb3f6ffd9bbe68c2fc70a96dd2a319a75cfd
muratavci05 <js.muratavci@gmail.com>
2023-04-23 02:14:30 +0300

    components modified

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,61 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png"
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import { auth } from "../../config/Config";
+import { Icon } from "react-icons-kit";
+import { shoppingCart } from "react-icons-kit/feather/shoppingCart";
 
-const Navbar = () => {
+const Navbar = ({ user }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth.signOut().then(() => {
+      navigate("/login");
+    });
+  };
   return (
     <div className="navbar-expand">
-    <div className="navbar">
-      <div className="leftside">
-        <div className="logo-brand"> 
-          <img src={logo} alt="logo" width="60"/>
-         </div>
-         </div>
-         <div className="rightside">
-        
-        <div><Link className="navlink text-primary" to={"signup"}>SIGN UP</Link></div>
-        <div><Link className="navlink text-primary" to={"login"}>LOGIN</Link></div>
-    
-         
-        
-      
-      
+      <div className="navbar">
+        <div className="leftside">
+          <div className="logo-brand">
+            <img src={logo} alt="logo" width="60" />
+          </div>
+        </div>
+        <div className="rightside">
+          {!user && 
+            <>
+              <div>
+                <Link className="navlink " to="signup">
+                  SIGN UP
+                </Link>
+              </div>
+              <div>
+                <Link className="navlink" to="login">
+                  LOGIN
+                </Link>
+              </div>
+            </>
+          }
+          {user && 
+            <>
+              <div>
+                <Link className="navlink" to="/">
+                  {user}
+                </Link>
+              </div>
+              <div className="cart-menu-btn">
+                <Link className="navlink" to="/cart">
+                  <Icon icon={shoppingCart} size={20} />
+                </Link>
+                {/* <span className="cart-indicator">{totalQty}</span> */}
+              </div>
+              <div className="btn btn-danger btn-md" onClick={handleLogout}>
+                LOGOUT
+              </div>
+            </>
+          }
+        </div>
       </div>
-     
-    </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Product from '../components/Product'
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/navbar/Navbar";
+import Product from "../components/Product";
+import { auth, firestore } from "../config/Config";
 
 const Home = () => {
+  //getting current user function
+
+  function GetCurrentUser() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      auth.onAuthStateChanged((user) => {
+        if (user) {
+
+          firestore.collection ("user").doc(user.uid).get().then(snapshot=>{
+            setUser(snapshot.data().FullName)
+          })
+        } else {
+          setUser(null);
+        }
+      });
+    }, [])
+    return user;
+  }
+
+  const user = GetCurrentUser();
+  /* console.log("kullanıcı", user); */
   return (
     <div>
-      <Navbar/>
-      <Product/>
+      <Navbar />
+      <Product />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
